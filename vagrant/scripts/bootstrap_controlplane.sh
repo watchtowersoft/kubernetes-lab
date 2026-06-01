e65a80f5ef11b70a401645454f5b70598c605861
Jason Guiton <jguiton@hctechpro.com>
2026-06-01 12:31:25 -0400

    updated kube version and generated upgrade script v1

--- vagrant/scripts/bootstrap_controlplane.sh
+++ vagrant/scripts/bootstrap_controlplane.sh
@@ -1,8 +1,8 @@
 ## ALL NODES
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y containerd kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
